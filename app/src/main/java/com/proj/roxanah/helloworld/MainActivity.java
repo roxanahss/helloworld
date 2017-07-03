8084aa170bb85463041263eaa672ef95ec396db3
Roxana Hossu <hossu.roxana-ext@groupehn.com>
2017-07-03 15:50:28 +0300

    test-br:commit 1

--- app/src/main/java/com/proj/roxanah/helloworld/MainActivity.java
+++ app/src/main/java/com/proj/roxanah/helloworld/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+	//comment onCreateOptionsMenu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
